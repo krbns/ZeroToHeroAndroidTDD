5fe8be508e615e7b4ab747b5795306e3c0e61f2e
Kurban <imangazaliev.kb@gmail.com>
2026-02-28 19:24:49 +0300

    code for task/011-increment

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,41 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+
+private const val COUNTER = "COUNTER"
 class MainActivity : AppCompatActivity() {
+    private var countTextView: TextView? = null
+    private var incrementButton: Button? = null
+
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(COUNTER)
+        }
+
+        countTextView = findViewById(R.id.countTextView)
+        incrementButton = findViewById(R.id.incrementButton)
+
+
+        countTextView?.text = counter.toString()
+
+        incrementButton?.setOnClickListener {
+            counter += 2
+            countTextView?.text = counter.toString()
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(COUNTER, counter)
+    }
+
 }
