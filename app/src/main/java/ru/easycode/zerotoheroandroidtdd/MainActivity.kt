5e41a7b74050c46f060e6293d8dfb8c69760734a
Kurban <imangazaliev.kb@gmail.com>
2026-02-28 19:06:57 +0300

    code for task/008-hide-text

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,39 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+
+private const val TITLE_VISIBILITY = "TITLE_VISIBILITY"
 class MainActivity : AppCompatActivity() {
+    private var titleTextView: TextView? = null
+
+    private var titleVisibility = true
+    private var changeButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            titleVisibility = savedInstanceState.getBoolean(TITLE_VISIBILITY)
+        }
+
+        titleTextView = findViewById(R.id.titleTextView)
+        changeButton = findViewById(R.id.hideButton)
+
+        titleTextView?.visibility = if (titleVisibility) View.VISIBLE else View.GONE
+
+        changeButton?.setOnClickListener {
+            titleTextView?.visibility = View.GONE
+            titleVisibility = false
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(TITLE_VISIBILITY, titleVisibility)
+    }
+
 }
