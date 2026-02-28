098059e056accc4282222d39f9cea1a1397f0258
Kurban <imangazaliev.kb@gmail.com>
2026-02-28 17:07:48 +0300

    code for task/006-change-text-runtime

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var titleTextView: TextView? = null
+    private var changeButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        titleTextView = findViewById(R.id.titleTextView)
+        changeButton = findViewById(R.id.changeButton)
+
+
+        changeButton?.setOnClickListener {
+            titleTextView?.text = "I am an Android Developer!"
+        }
     }
 }
