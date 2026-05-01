eb555836c71ba921f5cf4cf2fbd46b4b43f292ff
Kurban <imangazaliev.kb@gmail.com>
2026-05-01 18:57:44 +0300

    code for task/022-list

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,61 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var actionButton: Button? = null
+    private var inputEditText: TextInputEditText? = null
+    private var contentLayout: LinearLayout? = null
+
+    private val items = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        actionButton = findViewById(R.id.actionButton)
+        inputEditText = findViewById(R.id.inputEditText)
+        contentLayout = findViewById(R.id.contentLayout)
+
+        if (savedInstanceState != null) {
+            val savedItems = savedInstanceState.getStringArrayList("items")
+
+            savedItems?.forEach {
+                val textView = TextView(this).apply {
+                    this.text = it
+                    textSize = 18f
+                }
+
+                contentLayout?.addView(textView)
+            }
+        }
+
+        actionButton?.setOnClickListener {
+            val text = inputEditText?.text?.toString().orEmpty()
+
+            if (text.isNotBlank()) {
+                items.add(text)
+                inputEditText?.setText("")
+                val textView = TextView(this).apply {
+                    this.text = text
+                    textSize = 18f
+                }
+
+                contentLayout?.addView(textView)
+
+                inputEditText?.setText("")
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putStringArrayList("items", ArrayList(items))
     }
 }
