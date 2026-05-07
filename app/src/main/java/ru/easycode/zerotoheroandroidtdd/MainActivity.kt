30d71d740d57330295c1fe6a6dceedd8d687baa1
Kurban <imangazaliev.kb@gmail.com>
2026-05-07 12:57:40 +0300

    code for task/033-change-text-recreated

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,10 +3,17 @@ package ru.easycode.zerotoheroandroidtdd
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ru.easycode.zerotoheroandroidtdd.ui.theme.ZeroToHeroAndroidTDDTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +25,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //todo
+                    Column {
+                        val isClicked = rememberSaveable { mutableStateOf(false) }
+                        if (!isClicked.value) {
+                            Button(
+                                modifier = Modifier.size(100.dp),
+                                onClick = {
+                                    isClicked.value = true
+                                },
+                                content = {
+                                    Text(text = "Click me!")
+                                }
+                            )
+                        }
+
+                        if (isClicked.value) {
+                            Button(
+                                modifier = Modifier.size(100.dp),
+                                onClick = {
+                                    isClicked.value = false
+                                },
+                                content = {
+                                    Text(text = "That's right!")
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
